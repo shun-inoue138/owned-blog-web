3ec3c3aec5113eb43eaa2c416ccfd5de9b4bc420
shun-inoue138 <57622411+shun-inoue138@users.noreply.github.com>
2023-06-30 12:23:10 +0900

    add:ログインユーザーに権限が無い場合、投稿フォームを表示しない

--- src/pages/posts/create.tsx
+++ src/pages/posts/create.tsx
@@ -2,7 +2,16 @@ import PostForm from "@/components/PostForm";
 import { UserContainer } from "@/store/UserContainer";
 import React from "react";
 
-const create = () => {
+const Create = () => {
+  const { user } = UserContainer.useContainer();
+  console.log(user);
+
+  //// TODO:分岐条件これでいいか考える.万が一roleプロパティに”wiewer”(誤字)のような値が格納されていた場合、投稿ページが表示されてしまう。
+  if (user?.role === "viewer" || !user) {
+    //現状adminユーザーやcontributorユーザーでも一瞬このページが表示されてしまうが、取り敢えずOK
+    return <div>アクセス権限がありません</div>;
+  }
+
   return (
     <>
       {/* 編集画面の場合データをpropsで渡す。 */}
@@ -11,4 +20,4 @@ const create = () => {
   );
 };
 
-export default create;
+export default Create;
