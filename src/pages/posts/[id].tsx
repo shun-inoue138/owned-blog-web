8ec794282fc015eb38fab72ea5121b7400a8fcae
shun-inoue138 <57622411+shun-inoue138@users.noreply.github.com>
2023-06-30 16:24:25 +0900

    add:記事詳細ページの仮実装

---
+++ src/pages/posts/[id].tsx
@@ -0,0 +1,35 @@
+import { Post, PostAPI } from "@/api/PostAPI";
+import PostCard from "@/components/PostCard";
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const posts = await PostAPI.findAll();
+
+  const paths = posts.map((post) => ({
+    params: { id: post._id },
+  }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps<{ post: Post }> = async ({
+  params,
+}) => {
+  // TODO:赤線を削除するために無理やり型キャストしている。要精査。
+  const post = await PostAPI.findOne(params?.id as string);
+
+  return { props: { post } };
+};
+
+const PostDetail = ({
+  post,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  return (
+    <div>
+      {/* TODO: ここでPostDetailコンポーネントを呼び出す。とりあえずPostCardで代用 */}
+      <PostCard {...post} />
+    </div>
+  );
+};
+
+export default PostDetail;
