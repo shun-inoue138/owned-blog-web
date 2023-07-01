a1946d0652ee85677536b64927492917a6365552
shun-inoue138 <57622411+shun-inoue138@users.noreply.github.com>
2023-07-02 06:44:08 +0900

    add:一覧表示画面において「自身の投稿のみ表示」を可能に

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,6 +2,9 @@ import { GetStaticProps } from "next";
 import { Post, PostAPI } from "@/api/PostAPI";
 import PostCard from "@/components/PostCard";
 import Link from "next/link";
+import Header from "@/components/Header";
+import { useEffect, useMemo, useState } from "react";
+import { UserContainer } from "@/store/UserContainer";
 
 export const getStaticProps: GetStaticProps = async () => {
   const posts = await PostAPI.findAll();
@@ -21,13 +24,41 @@ type Props = {
 };
 
 const Index: React.FC<Props> = ({ posts }) => {
+  const { user } = UserContainer.useContainer();
+  const [postsToDisplay, setPostsToDisplay] = useState<Post[]>(posts);
+  const [shouldDisplayOnlyMyPosts, setShouldDisplayOnlyMyPosts] =
+    useState<boolean>(false);
+
+  useEffect(() => {
+    if (shouldDisplayOnlyMyPosts) {
+      setPostsToDisplay(posts.filter((post) => post.user._id === user?._id));
+    } else {
+      setPostsToDisplay(posts);
+    }
+  }, [shouldDisplayOnlyMyPosts]);
   return (
     <>
-      <div className="w-[96%] max-w-[1200px] mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2  gap-6">
-        {posts.map((post) => {
+      <Header />
+      {(user?.role === "admin" || user?.role === "contributor") && (
+        <div className=" ml-8 mt-7 sticky top-24 z-10">
+          <label>
+            <input
+              type="checkbox"
+              onChange={() => {
+                setShouldDisplayOnlyMyPosts((prev) => !prev);
+              }}
+            />
+            <span className="ml-2 ">自分の投稿のみ表示する</span>
+          </label>
+        </div>
+      )}
+
+      <div className="w-[96%] max-w-[1200px] mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2  gap-6 mt-24">
+        {postsToDisplay.map((post) => {
+          const isMyPost = Boolean(user?._id && user._id === post.user._id);
           return (
             <Link key={post._id} href={`/posts/${post._id}`}>
-              <PostCard {...post} />
+              <PostCard isMyPost={isMyPost} {...post} />
             </Link>
           );
         })}
