a459c2f9df2575a17ba4d4908b28302a3c3b840b
shun-inoue138 <57622411+shun-inoue138@users.noreply.github.com>
2023-07-02 17:11:55 +0900

    remove：不要なconsole.log

--- src/pages/posts/create.tsx
+++ src/pages/posts/create.tsx
@@ -5,7 +5,6 @@ import React from "react";
 
 const Create = () => {
   const { user } = UserContainer.useContainer();
-  console.log(user);
 
   //// TODO:分岐条件これでいいか考える.万が一roleプロパティに”wiewer”(誤字)のような値が格納されていた場合、投稿ページが表示されてしまう。
   if (user?.role === "viewer" || !user) {
