b6a02045cb1766ac460f6d7e3f3db40c64365c77
shun-inoue138 <57622411+shun-inoue138@users.noreply.github.com>
2023-07-02 17:09:59 +0900

    意図通りに動かないコンポーネント

---
+++ src/components/elements/StyledInput.tsx
@@ -0,0 +1,14 @@
+import React, { ComponentPropsWithRef, FC } from "react";
+
+const StyledInput: FC<
+  //classNameを拒絶して親からスタイリング出来なくする
+  Omit<ComponentPropsWithRef<"input">, "className">
+> = (props) => {
+  console.log(props);
+
+  return (
+    <input className="outline rounded-md py-1 md:py-2 px-4 w-full" {...props} />
+  );
+};
+
+export default StyledInput;
