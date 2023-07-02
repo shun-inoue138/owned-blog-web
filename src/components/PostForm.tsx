479406a80f494575e6d9330013c8f8999ae87b0c
shun-inoue138 <57622411+shun-inoue138@users.noreply.github.com>
2023-07-02 09:48:08 +0900

    微修正

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -43,7 +43,7 @@ export default function PostForm({ userId }: { userId: User["_id"] }) {
 
   const onSubmit = async (data: FormData) => {
     try {
-      console.log(data);
+      console.log(data.image);
       // TODO:編集の場合はPostAPI.updateを呼び出す.user:userIdに違和感。設計がそもそもおかしいかも。
       await PostAPI.create({ ...data, user: userId });
       // TODO:一覧画面or詳細画面に画面遷移
@@ -59,65 +59,62 @@ export default function PostForm({ userId }: { userId: User["_id"] }) {
   };
 
   return (
-    <div className="h-screen my-auto">
-      <form
-        className="p-4 max-w-[600px] mx-auto"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <h1 className="mb-5 text-3xl text-main_text font-bold text-center">
-          記事新規作成
-        </h1>
-        <label className="mb-6 block">
-          <h2>タイトル</h2>
-          <input className="w-full  p-2 rounded" {...register("title")} />
-          {errors?.title && (
-            <p className="text-accent">{errors.title.message}</p>
-          )}
-        </label>
-        {/* // TODO: 文字数カウンター */}
+    <form
+      className="p-4 max-w-[600px] mx-auto"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <h1 className="mb-5 text-3xl text-main_text font-bold text-center">
+        記事新規作成
+      </h1>
+      <label className="mb-6 block">
+        <h2>タイトル</h2>
+        <input className="w-full  p-2 rounded" {...register("title")} />
+        {errors?.title && <p className="text-accent">{errors.title.message}</p>}
+      </label>
+      {/* // TODO: 文字数カウンター */}
 
-        <label className="mb-6 block">
-          <h2>本文</h2>
-          <textarea
-            className="w-full  p-2 rounded mb-0"
-            {...register("content")}
-          />
-          {errors?.content && (
-            <p className="text-accent w-full">{errors.content.message}</p>
-          )}
-        </label>
-        {/* // TODO:文字数カウンター */}
-        <div className="flex justify-between ">
-          <div>
-            <label className="block">
-              <BiImageAdd className="text-3xl text-main_text" />
-              <input type="file" className="hidden" {...register("image")} />
-            </label>
-            {image?.name && (
-              <div className="flex items-center gap-2">
-                <p className="max-w-[10em] truncate">{image.name}</p>
-                <button
-                  onClick={() => {
-                    setValue("image", undefined);
-                  }}
-                >
-                  <ImCross className="text-accent" />
-                </button>
-              </div>
-            )}
-          </div>
-          <label>
-            <input type="checkbox" {...register("isPrivate")} />
-            <span className="ml-1 text-sub_text text-sm">公開しない</span>
+      <label className="mb-6 block">
+        <h2>本文</h2>
+        <textarea
+          className="w-full  p-2 rounded mb-0"
+          {...register("content")}
+        />
+        {errors?.content && (
+          <p className="text-accent w-full">{errors.content.message}</p>
+        )}
+      </label>
+      {/* // TODO:文字数カウンター */}
+      <div className="flex justify-between ">
+        <div>
+          <label className="block">
+            <BiImageAdd className="text-3xl text-main_text" />
+            <input type="file" className="hidden" {...register("image")} />
           </label>
+
+          {image?.[0]?.name && (
+            <div className="flex items-center gap-2">
+              <p className="max-w-[10em] truncate">{image?.[0]?.name}</p>
+              <button
+                onClick={() => {
+                  setValue("image", undefined);
+                }}
+              >
+                <ImCross className="text-accent" />
+              </button>
+            </div>
+          )}
         </div>
-        <button
-          className="w-full mt-6 py-2 px-4 rounded bg-main text-base_text"
-          type="submit"
-        >
-          {isPrivate ? "下書き保存" : "投稿"}
-        </button>
-      </form>
-    </div>
+        <label>
+          <input type="checkbox" {...register("isPrivate")} />
+          <span className="ml-1 text-sub_text text-sm">公開しない</span>
+        </label>
+      </div>
+      <button
+        className="w-full mt-6 py-2 px-4 rounded bg-main text-base_text"
+        type="submit"
+      >
+        {isPrivate ? "下書き保存" : "投稿"}
+      </button>
+    </form>
   );
 }
