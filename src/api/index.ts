4a144db4c3fffc98664af8de67094d0a51f7ecbb
shun-inoue138 <57622411+shun-inoue138@users.noreply.github.com>
2023-07-02 09:46:17 +0900

    add:画像送信用のaxios設定

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,16 @@
 import axios from "axios";
 
+const localUrl = "http://localhost:3005/";
+
 export const api = axios.create({
-  baseURL: "http://localhost:3005/",
+  baseURL: localUrl,
   headers: {
     "Content-Type": "application/json",
+  },
+});
+export const apiWithFile = axios.create({
+  baseURL: localUrl,
+  headers: {
+    "Content-Type": "multipart/form-data",
   },
 });
