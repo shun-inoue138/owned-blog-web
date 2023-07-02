35cda64f55fdacd8a9e5e1c08f8dcfef9cf824a9
shun-inoue138 <57622411+shun-inoue138@users.noreply.github.com>
2023-07-02 17:10:31 +0900

    token対応

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,18 @@ import axios from "axios";
 
 const localUrl = "http://localhost:3005/";
 
+const getToken = () => {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("token");
+    if (token) {
+      return token;
+    } else {
+      return "";
+    }
+  }
+  return "";
+};
+
 export const api = axios.create({
   baseURL: localUrl,
   headers: {
@@ -14,3 +26,33 @@ export const apiWithFile = axios.create({
     "Content-Type": "multipart/form-data",
   },
 });
+
+api.interceptors.request.use((config) => {
+  config.headers["Authorization"] = `bearer ${getToken()}`;
+  config.headers["Content-Type"] = "application/json";
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+apiWithFile.interceptors.request.use((config) => {
+  config.headers["Authorization"] = `bearer ${getToken()}`;
+  config.headers["Content-Type"] = "multipart/form-data";
+  return config;
+});
+
+apiWithFile.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
