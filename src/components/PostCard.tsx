cccbab71ff86d166f7d9b0a6209b8e3162d2f76e
shun-inoue138 <57622411+shun-inoue138@users.noreply.github.com>
2023-07-03 17:01:54 +0900

    add:更新日欄の追加など

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -6,10 +6,7 @@ import dayjs from "dayjs";
 import clsx from "clsx";
 import Base64Image from "./Base64Image";
 
-const PostCard: FC<Post & { isMyPost?: boolean }> = ({
-  isMyPost = false,
-  ...post
-}) => {
+const PostCard: FC<Post> = ({ ...post }) => {
   return (
     <div>
       <div className="relative w-full h-auto">
@@ -21,23 +18,24 @@ const PostCard: FC<Post & { isMyPost?: boolean }> = ({
         <Divider />
         <p className="h-[5em] truncate">{post.content}</p>
         <Divider />
-        <div className="flex justify-between items-center">
-          <div className="flex gap-2 items-center">
-            <span className="bg-main text-base_text rounded-md p-2">
-              投稿日
-            </span>
-            <p>{dayjs(post.createdAt).format("YYYY/MM/DD HH:mm")}</p>
+        <div className="flex justify-between items-end">
+          <div>
+            <div className="flex gap-2 items-center">
+              <span className="bg-main text-base_text rounded-md px-2 py-1">
+                投稿日
+              </span>
+              <p>{dayjs(post.createdAt).format("YYYY/MM/DD HH:mm")}</p>
+            </div>
+            <div className="flex gap-2 items-center mt-1">
+              <span className="bg-main text-base_text rounded-md px-2 py-1">
+                更新日
+              </span>
+              <p>{dayjs(post.updatedAt).format("YYYY/MM/DD HH:mm")}</p>
+            </div>
           </div>
           <p className="text-right">
             <span className="mr-1 text-sm text-sub_text">by</span>
-            <span
-              className={clsx(
-                "text-lg",
-                isMyPost && "text-xl text-success font-semibold"
-              )}
-            >
-              {post.user.name}
-            </span>
+            <span className="text-lg">{post.user.name}</span>
           </p>
         </div>
       </div>
